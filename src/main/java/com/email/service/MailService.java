2be56cb279651ae38f3af5fb9b99cfe697229cd2
Imalka Kalani <imalka.kalani@travisperkins.co.uk>
2019-09-03 11:54:36 +0530

    Pushed test email source

---
+++ src/main/java/com/email/service/MailService.java
@@ -0,0 +1,63 @@
+package com.email.service;
+
+import java.io.IOException;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+
+@SpringBootApplication
+public class MailService implements CommandLineRunner {
+@Autowired 
+private JavaMailSender javaMailSender;
+
+public static void main(String[] args) {
+    SpringApplication.run(MailService.class, args);
+}
+
+
+@Override 
+public void run(String... args) throws MessagingException,IOException {
+  
+    System.out.println("Sending Email...");
+  
+    sendEmailWithoutAttachment(); 
+    //sendEmailWithAttachment();
+  
+    System.out.println("Email Sent...!");
+ }
+  
+  void sendEmailWithoutAttachment() {
+  
+  SimpleMailMessage message = new SimpleMailMessage();
+  message.setTo("imarathnayaka91@gmail.com");
+  message.setSubject("Testing from Spring Boot");
+  message.setText("Hello World \n Spring Boot Email");
+  
+  javaMailSender.send(message);
+  
+  }
+  
+  void sendEmailWithAttachment() throws MessagingException, IOException {
+  
+  MimeMessage message = javaMailSender.createMimeMessage(); MimeMessageHelper
+  helper = new MimeMessageHelper(message, true);
+  helper.setTo("imarathnayaka91@gmail.com");
+  helper.setSubject("Testing from Spring Boot");
+  
+  helper.setText("<B>Image attached in the email..!</B>", true);
+  helper.addAttachment("Spring.png", new ClassPathResource("ImageToUpload.png"));
+  
+  javaMailSender.send(message);
+  
+  }
+
+}
